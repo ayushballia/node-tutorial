76b9377d0406a5c336e9537af0b5545f71cdb659
ayushballia <ayushtiwari@live.in>
2022-03-12 11:44:34 +0530

    internal modules

---
+++ app.js
@@ -0,0 +1,14 @@
+// npm - global command, comes with node
+// npm --version
+
+// local dependency - use it only in perticular project
+// npm i <packageName>
+
+// global dependency - use it in any project
+// npm install -g <pakageName>
+// sudo install -g <packageName> (mac)
+
+// package.json - manifest file (stores important info about project/package)
+// manual approach (create package.json in root, create properties etc)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything default)

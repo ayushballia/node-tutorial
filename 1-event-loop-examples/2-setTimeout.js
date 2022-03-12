3284d3129dc17118d07d5bade3ec2cdd8c18daf1
ayushballia <ayushtiwari@live.in>
2022-03-12 21:18:47 +0530

    event loop

--- 1-event-loop-examples/2-setTimeout.js
+++ 1-event-loop-examples/2-setTimeout.js
@@ -2,4 +2,4 @@
 console.log("first");
 setTimeout(() => console.log("second"), 0);
 console.log("third");
- 
+// completed and exited operating system process
--- 12-http.js
+++ 12-http.js
@@ -2,10 +2,10 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
-    res.end("welcome to our homepage");
+    return res.end("welcome to our homepage");
   }
   if (req.url === "/about") {
-    res.end("Here is our short history");
+    return res.end("Here is our short history");
   }
   res.end(`
     <h1>Oops!!!</h1>
